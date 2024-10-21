2f18826a6c9a385b9af2f5ac41e639df16db8e83
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-21 09:39:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,8 @@ def add_text_to_image(image, text, coordinates,
     x, y = coordinates
     for line in wrapped_text:
         draw.text((x, y), line.strip(), font=font, fill=color)
+        y += font.getbbox(line.strip())[3]  # Move to the next line
+
 
                       
 
