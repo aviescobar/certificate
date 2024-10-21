222b2044adca423029ebb8fe67c88fec9bdee444
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-20 22:24:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ current_path = Path.cwd()
 pdfs_file_path = current_paht / "pdfs"
 fronts_files_path = current_path / "fonts"
 images_files_path = current_path / "images"
+imsges_tmp_files = images_files_path / "tmp"
 
 
 
