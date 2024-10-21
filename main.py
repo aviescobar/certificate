e02f967836fcf003bf4f8798146c11580d36b2f1
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-21 09:36:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -41,8 +41,11 @@ def add_text_to_image(image, text, coordinates,
                       max_width=2000, # Average width of the image
                       show=False):
     draw = ImageDraw.Draw(image)
+
     font = ImageFont.truetype(str(font_path), font_size)
     wrapped_text = wrap_text(text, font, max_width)
+
+    x, y = coordinates
                       
 
 
